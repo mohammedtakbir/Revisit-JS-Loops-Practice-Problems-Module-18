7eb04e13c6e518e647102c80fedf5fe23596c44a
mohammedtakbir <takbirhossain678@gmail.com>
2023-02-11 08:49:55 +0600

    explore for loop

---
+++ for.js
@@ -0,0 +1,26 @@
+//* while loop
+// let count = 0;
+// while(count < 10){
+//     console.log(count);
+//     count++;
+// };
+
+//* for loop
+// for(let count = 0; count < 10; count++){
+//     console.log(count)
+// }
+
+//* simple version of for loop
+// for(let i = 0; i < 10; i++){
+//     console.log(i)
+// }
+
+//* get the even value from 1 to 10 using for loop
+// for (let i = 0; i <= 9; i += 2) {
+//     console.log(i)
+// }
+
+//* get the odd value from 1 to 10 using for loop
+for (let i = 1; i <= 10; i += 2) {
+    console.log(i)
+}
