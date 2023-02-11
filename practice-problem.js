f7e99e7248e379b5f3acc275ac671c766ecf147e
mohammedtakbir <takbirhossain678@gmail.com>
2023-02-11 20:32:22 +0600

    more problem practice on module-18.5

--- practice-problem.js
+++ practice-problem.js
@@ -38,20 +38,96 @@
 // console.log(result)
 
 //* problem-04
-let num1 = 8;
-let num2 = 98;
-let num3 = 67;
+// let num1 = 8;
+// let num2 = 98;
+// let num3 = 67;
 
-if(num1 > num2){
-    if(num1 > num3){
-        console.log(num1)
+// if(num1 > num2){
+//     if(num1 > num3){
+//         console.log(num1)
+//     }else{
+//         console.log(num3)
+//     }
+// }else{
+//     if(num2 > num3){
+//         console.log(num2)
+//     }else{
+//         console.log(num3)
+//     }
+// }
+
+//* ২৪. তোমার কাছে: ৮০০০০ টাকার বেশি হলে তুমি mac কিনবে, ৬০ টাকার বেশি হলে gaming ল্যাপটপ কিনবে, ৪০ হাজার টাকার বেশি হলে lenovo yoga কম্পিউটার কিনবে , ২০ হাজার টাকার বেশি হলে পুরান ল্যাপটপ কিনবে। না হয় তুমি মোবাইল দিয়ে কাজ চালাবে। 
+
+// const myBudget = 65000;
+// const mac = 80000;
+// const gamingLaptop = 60000;
+// const lenovo = 40000;
+// const usedLaptop = 20000
+
+// if(myBudget >= mac){
+//     console.log('Buy Mac')
+// }else if(myBudget >= gamingLaptop && myBudget < mac){
+//     console.log('Buy Gaming Laptop')
+// }else if(myBudget >= lenovo && myBudget < gamingLaptop){
+//     console.log('Buy Lenovo')
+// }else if(myBudget >= usedLaptop && myBudget < lenovo){
+//     console.log('Buy Used Laptop')
+// }
+
+//* ২৯. একটা কোড লিখে ৫৮ থেকে ৯৮ পর্যন্ত যত সংখ্যা আছে সেগুলাকে দেখাও 
+// let i = 0;
+// while (i <= 98) {
+//     if (i >= 58 && i <= 98) {
+//         console.log(i)
+//     }
+//     i++;
+// }
+
+//* ৩০.একটা কোড লিখে ৪১২ থেকে ৪৫৬ পর্যন্ত যত জোর সংখ্যা আছে সেগুলাকে দেখাও
+// let i = 0;
+// while (i <= 456) {
+//     if (i >= 412 && i <= 456) {
+//         console.log(i)
+//     }
+//     i +=2
+// }
+
+//* ৩১. একটা কোড লিখে ৫৮১ থেকে ৬২৩ পর্যন্ত যত বিজোড় সংখ্যা আছে সেগুলাকে দেখাও 
+
+// for (let i = 1; i <= 623; i += 2) {
+//     if(i >= 581 && i <= 623){
+//         console.log(i)
+//     }
+// }
+
+//* ৩৫. একটা ফর লুপ চালাও। ৩০ থেকে ৮৬ পর্যন্ত। আর এই লুপ ৪৪ এ গেলে ব্রেক করবে। সেই জিনিস কোড করে দেখাও 
+
+// for (let i = 0; i <= 86; i++) {
+//     if (i >= 30 && i <= 86) {
+//         if (i === 44) {
+//             console.log(i)
+//             break;
+//         }
+//     }
+// }
+
+//* ৩৬. তোমার যত বই আছে সেগুলার দাম নিয়ে একটা array লিখে ফেলো। যে বই গুলোর দাম ২০০ টাকার উপরে সেগুলাকে স্কিপ করবে। অর্থাৎ সেগুলাকে আউটপুট হিসেবে দেখাবে না। বাকিদের কে আউটপুট হিসেবে দেখাবে। দেখো করতে পারো কিনা। 
+
+const booksPrice = [100, 351, 214, 147, 159, 58, 258, 123];
+// let i = 0;
+// while (i < booksPrice.length) {
+//     const bookPrice = booksPrice[i];
+//     if (bookPrice > 200) {
+//         continue
+//     }
+//     i++;
+//     console.log(bookPrice)
+// }
+for(let i = 0; i < booksPrice.length; i++){
+    const bookPrice = booksPrice[i];
+    if(bookPrice > 200){
+        continue;
     }else{
-        console.log(num3)
-    }
-}else{
-    if(num2 > num3){
-        console.log(num2)
-    }else{
-        console.log(num3)
+        console.log(bookPrice)
     }
 }
